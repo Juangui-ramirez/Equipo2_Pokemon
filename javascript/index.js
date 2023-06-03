63920f88edef82a73a2f49a9af499149501d74c4
Juangui <juan-ramirez04@hotmail.com>
2023-06-03 12:07:45 -0500

    update

--- javascript/index.js
+++ javascript/index.js
@@ -2,13 +2,14 @@ let url = "https://pokeapi.co/api/v2/pokemon";
 
 const getPokemon = async () => {
     try{
-        const res = await fetch(`${url}/pokemon`);
+        const res = await fetch(`${url}?limit=25`);
         const data = await res.json();
         data.results.forEach(async (pokemon) => {
             const name = pokemon.name;
-            document.querySelector(".namePoke").appendChild(".card")
+            document.querySelector(".namePoke").textContent = name;
         });
     } catch(error){
         alert ("error");
     }
 }
+getPokemon();
